42451254d9311e61a8eaaa99d4120fa2ee93e023
Lukasz <lukaszandruchow@gmail.com>
2017-06-15 15:43:47 +0100

    Working countdown after button click

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,18 @@
 window.onload = function ()
 {
-window.alert("hey!");
 
+	$("#start").on("click", function() {
+		var n = 60;
+		var tm = setInterval(countDown,1000);
 
+		function countDown(){
+		   n--;
+		   if(n == 0){
+		      clearInterval(tm);
+		   }
+		   console.log(n);
+		}
+	});
 }
 
 
-setInterval ( "doSomething()", 5000 );
-
-function doSomething ( )
-{
-  // (do something here)
-}
