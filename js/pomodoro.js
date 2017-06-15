2702e1068dc13cd9bccb2bc2d87535401d8d7056
Lukasz <lukaszandruchow@gmail.com>
2017-06-15 16:15:45 +0100

    working timer - visible countdown

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -4,12 +4,19 @@ window.onload = function ()
 	$("#start").on("click", function() {
 		var n = 60;
 		var tm = setInterval(countDown,1000);
-
+		var tleft = 0;
 		function countDown(){
 		   n--;
+		   tleft = n;
+    	   document.getElementById("left").innerHTML = tleft;
+
+		   //$("#left").innerHTML = n;
+
 		   if(n == 0){
 		      clearInterval(tm);
+		      
 		   }
+		   
 		   console.log(n);
 		}
 	});
