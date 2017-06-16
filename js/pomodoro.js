815604e2f74488ab8a91e7ca95cbd74ae58f209f
Lukasz <lukaszandruchow@gmail.com>
2017-06-16 14:51:21 +0100

    Both timers working

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -15,12 +15,30 @@ window.onload = function (){
     	   document.getElementById("left").innerHTML = minutes.substr(-2) + ":" + seconds.substr(-2);
 		   if(n == 0){
 		      clearInterval(tm);
-		      
 		   }
-		   
 		   //console.log(n);
 		}
 	});
+
+	$("#start2").on("click", function() {
+
+		btime = document.getElementById("work").value//getting value of user's input
+		var n = btime*60;//n is seconds for our function so we multiply by 60
+
+		var tm = setInterval(countDown,1000);
+		var tleft = 0;
+		function countDown(){
+		   n--;
+		   var minutes = "0" + Math.floor(n / 60);
+		   var seconds = "0" + (n - minutes * 60);
+    	   document.getElementById("right").innerHTML = minutes.substr(-2) + ":" + seconds.substr(-2);
+		   if(n == 0){
+		      clearInterval(tm);
+		   }
+		   //console.log(n);
+		}
+	});
+
 
 
 
