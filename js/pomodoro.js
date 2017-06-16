d098530922fba6fda13a9b95d91ef8eaec41bc1c
Lukasz <lukaszandruchow@gmail.com>
2017-06-16 14:36:32 +0100

    Countdown in mm:ss format

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -4,13 +4,15 @@ window.onload = function (){
 
 		btime = document.getElementById("break").value//getting value of user's input
 		var n = btime*60;//n is seconds for our function so we multiply by 60
-		
+
 		var tm = setInterval(countDown,1000);
 		var tleft = 0;
 		function countDown(){
 		   n--;
-		   tleft = n;
-    	   document.getElementById("left").innerHTML = tleft;
+		   var minutes = "0" + Math.floor(n / 60);
+		   var seconds = "0" + (n - minutes * 60);
+		   //tleft = n;
+    	   document.getElementById("left").innerHTML = minutes.substr(-2) + ":" + seconds.substr(-2);;
 
 		   //$("#left").innerHTML = n;
 
@@ -25,7 +27,7 @@ window.onload = function (){
 }
 
 /*
-var minutes = "0" + Math.floor(time / 60);
-var seconds = "0" + (time - minutes * 60);
+var minutes = "0" + Math.floor(n / 60);
+var seconds = "0" + (n - minutes * 60);
 return minutes.substr(-2) + ":" + seconds.substr(-2);
 */
